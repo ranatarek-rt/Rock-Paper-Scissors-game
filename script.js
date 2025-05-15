7543ba6a1537d1bbcafbb13f41a242e0477deea5
ranatarek-rt <rana195387@feng.bu.edu.eg>
2025-05-15 06:47:09 +0200

    images and css style

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ function gameStart(playerChoice){
     console.log(computerChoice);
     let computerMove = getMoveName(computerChoice);
     let playerMove = getMoveName(playerChoice);
-    document.querySelector('.js-move').innerHTML=`Player : ${playerMove} vs Computer : ${computerMove}`;
+    let move= document.querySelector('.js-move');
+    move.classList.add('p-move');
+    move.innerHTML=`Player : ${playerMove} vs Computer : ${computerMove}`;
+
+    document.querySelector('.js-div').classList.add('result-container');;
+    
     if(playerChoice===computerChoice){
         score.ties++;
         displayText('Tie');
@@ -33,14 +38,19 @@ function resetScore(){
     score.wins=0;
     score.ties=0;
     localStorage.setItem("score", JSON.stringify(score));
-    document.querySelector('.js-move').innerHTML='';
-    document.querySelector('.js-res').innerHTML='';
+    let move = document.querySelector('.js-move');
+    move.innerHTML='';
+    move.classList.remove('p-move');
+    let res= document.querySelector('.js-res');
+    res.innerHTML='';
+    res.classList.remove('p-res');
+    document.querySelector('.js-div').classList.remove('result-container');;
     updateScore();
     alert('the score is reseted');
 }
 
 function updateScore(){
-    document.querySelector('.js-score-res').innerHTML = ` wins:${score.wins}  loses:${score.loses}  ties:${score.ties}`;
+    document.querySelector('.js-score-res').innerHTML = ` 🏆 Wins: ${score.wins} &nbsp;&nbsp; ❌ Loses: ${score.loses} &nbsp;&nbsp; 🤝 Ties: ${score.ties}`;
 
 }
 function getMoveName(move){
@@ -52,5 +62,8 @@ function getMoveName(move){
     }
 }
 function displayText(text){
-    document.querySelector('.js-res').innerHTML=text;
+   let res =  document.querySelector('.js-res');
+   res.classList.add('p-res')
+   res.innerHTML=text;
+
 }
